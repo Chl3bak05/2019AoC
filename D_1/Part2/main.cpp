a7d73c0a14edaedcb6d01660e2e71005a44763ef
Mateusz Chlebosz <chleboszmateusz@gmail.com>
2019-12-01 20:33:38 +0100

    Day 1

--- D_1/Part2/main.cpp
+++ D_1/Part2/main.cpp
@@ -2,16 +2,25 @@
 
 using namespace std;
 
+int calculate_fuel(int a)
+{
+    a /= 3;
+    a -= 2;
+    if (a <= 0)
+        return 0;
+    else
+        return a + calculate_fuel(a);
+}
+
 int main(int argc, char const *argv[])
 {
     int sum = 0;
     int temp;
+
     for (short i = 0; i < 100; i++)
     {
         cin >> temp;
-        temp = temp / 3;
-        temp = temp - 2;
-        sum += temp;
+        sum += calculate_fuel(temp);
     }
 
     cout << sum;
